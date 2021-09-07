56d27160c08c466c934e39d22e27b00173195098
Jeff Dobson <jeff.dobson@10thmagnitude.com>
2021-09-07 12:55:04 -0400

    Added azurerm version to provider block

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     azurerm = {
         source  = "hashicorp/azurerm"
-        version = "=2.46.0"
+        version = "~> 2.49.0"
     }
   }
   backend "azurerm" {
@@ -31,6 +31,7 @@ terraform {
 
 provider "azurerm" {
   features {}
+  version = "~> 2.49.0"
 }
 
 resource "azurerm_resource_group" "rg" {
